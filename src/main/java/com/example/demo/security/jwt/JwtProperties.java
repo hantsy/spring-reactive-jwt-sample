5dec534367e748da7ef993a206bc667e838112c6
hantsy <hantsy@gmail.com>
2020-06-02 14:40:48 +0800

    add tests for JwtTokenProvider

--- src/main/java/com/example/demo/security/jwt/JwtProperties.java
+++ src/main/java/com/example/demo/security/jwt/JwtProperties.java
@@ -1,13 +1,19 @@
 package com.example.demo.security.jwt;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @ConfigurationProperties(prefix = "jwt")
 @Data
 public class JwtProperties {
 
-    private String secretKey = "flzxsqcysyhljt";
+    private String secretKey = "rzxlszyykpbgqcflzxsqcysyhljt";
 
     //validity in milliseconds
     private long validityInMs = 3600000; // 1h
---
+++ src/test/java/com/example/demo/JwtTokenProviderTest.java
@@ -0,0 +1,72 @@
+package com.example.demo;
+
+import com.example.demo.security.jwt.JwtProperties;
+import com.example.demo.security.jwt.JwtTokenProvider;
+import io.jsonwebtoken.JwtException;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Slf4j
+public class JwtTokenProviderTest {
+
+    private JwtTokenProvider jwtTokenProvider;
+
+    private static String TEST_USER = "user";
+    private static String TEST_ROLE_NAME = "ROLE_USER";
+
+    @BeforeEach
+    public void setup() {
+        JwtProperties properties = new JwtProperties();
+        log.debug("jwt properties::" + properties);
+        this.jwtTokenProvider = new JwtTokenProvider(properties);
+
+        assertNotNull(this.jwtTokenProvider);
+        this.jwtTokenProvider.init();
+    }
+
+    @Test
+    public void testGenerateAndParseToken() {
+        String token = generateToken(TEST_USER, TEST_ROLE_NAME);
+        log.debug("generated jwt token::" + token);
+        var auth = this.jwtTokenProvider.getAuthentication(token);
+        var principal = (UserDetails) auth.getPrincipal();
+        assertThat(principal.getUsername()).isEqualTo(TEST_USER);
+        assertThat(principal.getAuthorities().stream().map(a -> a.getAuthority()).collect(Collectors.toList())).contains(TEST_ROLE_NAME);
+    }
+
+    @Test
+    public void testGenerateAndParseToken_withoutRoles() {
+        String token = generateToken(TEST_USER);
+        log.debug("generated jwt token::" + token);
+        var auth = this.jwtTokenProvider.getAuthentication(token);
+        var principal = (UserDetails) auth.getPrincipal();
+        assertThat(principal.getUsername()).isEqualTo(TEST_USER);
+        assertThat(principal.getAuthorities()).isEmpty();
+    }
+
+    @Test
+    public void testParseTokenException() {
+        String token = "anunknowtoken";
+        assertThatThrownBy(()-> this.jwtTokenProvider.getAuthentication(token)).isInstanceOf(JwtException.class);
+    }
+
+    private String generateToken(String username, String... roles) {
+        Collection<? extends GrantedAuthority> authorities = AuthorityUtils.createAuthorityList(roles);
+        var principal = new User(username, "password", authorities);
+        var usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(principal, null, authorities);
+        return this.jwtTokenProvider.createToken(usernamePasswordAuthenticationToken);
+    }
+}
