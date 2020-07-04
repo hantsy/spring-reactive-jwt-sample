f9a43554b834aa9912d787631edd656aa2122e20
hantsy <hantsy@gmail.com>
2020-07-04 17:55:21 +0800

    fix: add formatter off/on to make some codes more readable

--- src/main/java/com/example/demo/DataInitializer.java
+++ src/main/java/com/example/demo/DataInitializer.java
@@ -32,22 +32,50 @@ class DataInitializer {
 	@EventListener(value = ApplicationReadyEvent.class)
 	public void init() {
 		log.info("start data initialization...");
-		var initPosts = this.users.deleteAll().thenMany(Flux.just("user", "admin").flatMap(username -> {
-			List<String> roles = "user".equals(username) ? Arrays.asList("ROLE_USER")
-					: Arrays.asList("ROLE_USER", "ROLE_ADMIN");
 
-			User user = User.builder().roles(roles).username(username).password(passwordEncoder.encode("password"))
-					.email(username + "@example.com").build();
-			return this.users.save(user);
-		}));
+		//@formatter:off
+		var initPosts = this.users.deleteAll()
+			.thenMany(
+					Flux.just("user", "admin")
+					.flatMap(username -> {
+						List<String> roles = "user".equals(username) ?
+								Arrays.asList("ROLE_USER"): Arrays.asList("ROLE_USER", "ROLE_ADMIN");
 
+						User user = User.builder()
+								.roles(roles)
+								.username(username)
+								.password(passwordEncoder.encode("password"))
+								.email(username + "@example.com")
+								.build();
+
+						return this.users.save(user);
+					})
+			);
+		//@formatter:on
+
+		//@formatter:off
 		var initUsers = this.posts.deleteAll()
-				.thenMany(Flux.just("Post one", "Post two").flatMap(title -> this.posts.save(Post.builder().title(title)
-						.content("content of " + title).status(Post.Status.PUBLISHED).build())));
+			.thenMany(
+					Flux.just("Post one", "Post two")
+						.flatMap(title ->
+							this.posts.save(Post.builder()
+								.title(title)
+								.content("content of " + title)
+								.status(Post.Status.PUBLISHED)
+								.build()
+							)
+						)
+			);
+		//@formatter:on
 
-		initPosts.doOnSubscribe(data -> log.info("data:" + data)).thenMany(initUsers).subscribe(
+		//@formatter:off
+		initPosts.doOnSubscribe(data -> log.info("data:" + data))
+			.thenMany(initUsers)
+			.subscribe(
 				data -> log.info("data:" + data), err -> log.error("error:" + err),
-				() -> log.info("done initialization..."));
+				() -> log.info("done initialization...")
+			);
+		//@formatter:on
 	}
 
 }
